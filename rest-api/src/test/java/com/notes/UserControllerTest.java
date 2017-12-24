b042ceee232d20f87dd300663ec8861bc0c09f78
Dmitry Stoyanov <stoyanov.dmt@gmail.com>
2017-12-24 13:32:55 +0200

    UserControllerTest are completed

--- rest-api/src/test/java/com/notes/UserControllerTest.java
+++ rest-api/src/test/java/com/notes/UserControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.notes.config.DataConfiguration;
 import com.notes.config.RestConfig;
 import com.notes.config.WebApp;
+import com.notes.entity.Note;
 import com.notes.entity.User;
 import com.notes.service.UserService;
 import org.ajbrown.namemachine.Name;
@@ -20,6 +21,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.sql.SQLException;
+
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -42,22 +47,32 @@ public class UserControllerTest {
 	private UserService userService;
 
 	private int id;
-	private String username;
+	private final String username = "username1";
 
 	private User testUser;
-	private String jsonTestUser;
 
 	@Before
 	public void setUp() {
-		id = 1;
-		username = "lindastaley";
+
+		User user;
+
+		try {
+			user = userService.getByUsername(username);
+			id = user.getId();
+		} catch (Exception ignore) {
+
+			user = new User(username, "pass", "mail@mail.me");
+			Note note1 = new Note("text1", user);
+			Note note2 = new Note("text2", user);
+			user.setNotes(Arrays.asList(note1, note2));
+
+			userService.save(user);
+
+			user = userService.getByUsername(username);
+			id = user.getId();
+		}
 
 		testUser = new User("test", "pass", "test@email.com");
-		try {
-			jsonTestUser = new ObjectMapper().writeValueAsString(testUser);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
 
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
@@ -92,11 +107,51 @@ public class UserControllerTest {
 
 	@Test
 	public void addUser() throws Exception {
+
+		String jsonUser = objectToJson(testUser);
+
 		mockMvc.perform(post("/users")
-				                .content(jsonTestUser)
+				                .content(jsonUser)
 				                .contentType(MediaType.APPLICATION_JSON_UTF8)
 				                .accept(MediaType.APPLICATION_JSON_UTF8))
 				.andExpect(status().isOk());
-//		userService.delete(testUser);
+	}
+
+	@Test
+	public void updateUser() throws Exception {
+
+		String name = testUser.getUsername();
+
+		User user = userService.getByUsername(name);
+		user.setEmail("updated@mail.com");
+
+		String jsonUser = objectToJson(user);
+		testUser = user;
+
+		mockMvc.perform(put("/users/" + name)
+				                .content(jsonUser)
+				                .contentType(MediaType.APPLICATION_JSON_UTF8)
+				                .accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void deleteUser() throws Exception {
+
+		String name = testUser.getUsername();
+
+		mockMvc.perform(delete("/users/" + name)
+				                .contentType(MediaType.APPLICATION_JSON_UTF8)
+				                .accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk());
+	}
+
+	public String objectToJson(Object object) {
+		try {
+			return new ObjectMapper().writeValueAsString(object);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
