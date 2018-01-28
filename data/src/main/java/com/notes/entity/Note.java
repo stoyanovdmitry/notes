66978e2e7ecb0c0dc100a2524df9bd1277363a87
stoyanovdmitry <stoyanov.dmt@gmail.com>
2018-01-28 13:21:06 +0200

    changed Note.text columnDefinition to 'TEXT'

--- data/src/main/java/com/notes/entity/Note.java
+++ data/src/main/java/com/notes/entity/Note.java
@@ -11,7 +11,7 @@ public class Note implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "TEXT")
 	private String text;
 
 	@ManyToOne
